677a3cea550861f8a1638d5f38f46dbb31eb75d2
CCA2878 <1054331608@qq.com>
2023-09-17 04:07:30 +0800

    Add requirements.txt & Clean codes

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ MAIN_PATH = os.path.dirname(os.path.abspath(__file__))
 
 if __name__ == '__main__':
     # global MAIN_PATH
-    os.environ["QTWEBENGINE_REMOTE_DEBUGGING"] = "127.0.0.1:9222"
+    # os.environ["QTWEBENGINE_REMOTE_DEBUGGING"] = "127.0.0.1:9222"
     app = QApplication(sys.argv)
     mW = ui.MainWindowUi()
     sys.exit(app.exec())
